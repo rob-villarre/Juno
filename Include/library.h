4b5fa6f5b44d22f7cb1ba6669dfa58d2ad3ce935
Roberto Villarreal Andrade <rob.villarre@gmail.com>
2025-11-29 13:37:13 -0700

    initial commit

---
+++ Include/library.h
@@ -0,0 +1,6 @@
+#ifndef JUNO_LIBRARY_H
+#define JUNO_LIBRARY_H
+
+void hello();
+
+#endif // JUNO_LIBRARY_H
---
+++ Source/library.cpp
@@ -0,0 +1,8 @@
+#include "../Include/library.h"
+
+#include <iostream>
+
+void hello()
+{
+    std::cout << "Hello, World!" << std::endl;
+}
